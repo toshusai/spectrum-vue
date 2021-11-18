bccc97488d99f785c22dd99fe9c606f8884c870e
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-18 20:13:06 +0900

    Add SpActionBar

---
+++ src/components/SpActionBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>TODO</div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component }from "vue-property-decorator";
+
+@Component({})
+export default class SpActionBar extends Vue {
+
+}
+</script>
