1b0b78ebbb19ed8bf38afa05ef6a844c3682e217
toshusai <toshusai.net@gmail.com>
2021-11-18 13:00:29 +0900

    Add top examples

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -33,6 +33,95 @@
       </li>
     </ul>
     <div>
+      <sp-split-view :vertical="true">
+        <sp-split-view-pane style="padding: 8px">
+          <div class="margin">
+            <sp-action-button> ActionButton </sp-action-button>
+          </div>
+          <div class="margin">
+            <sp-action-group :compact="true">
+              <sp-action-button :item="true">Action</sp-action-button>
+              <sp-action-button :item="true">Group</sp-action-button>
+            </sp-action-group>
+          </div>
+          <div class="margin">
+            <sp-checkbox> Checkbox </sp-checkbox>
+          </div>
+          <div class="margin">
+            <sp-textfield placeholder="Textfield"> </sp-textfield>
+          </div>
+          <div class="margin">
+            <sp-text-area placeholder="TextArea"> </sp-text-area>
+          </div>
+          <div class="margin">
+            <sp-help-text>Helptext</sp-help-text>
+          </div>
+          <div class="margin">
+            <sp-menu>
+              <sp-menu-item>MenuItem 1</sp-menu-item>
+              <sp-menu-item>MenuItem 2</sp-menu-item>
+            </sp-menu>
+          </div>
+          <div class="margin">
+            <sp-link>link</sp-link>
+          </div>
+          <div class="margin">
+            <sp-divider></sp-divider>
+          </div>
+          <div class="margin">
+            <sp-tag label="Tag" size="S"></sp-tag>
+            <sp-tag label="Tag2" :selected="true" size="S"></sp-tag>
+            <sp-tag label="Tag3" :invalid="true" size="S"></sp-tag>
+          </div>
+          <div class="margin">
+            <sp-icon name="Cloud"></sp-icon>
+            <sp-icon name="Erase"></sp-icon>
+            <sp-icon name="Home"></sp-icon>
+          </div>
+          <div class="margin">
+            <sp-table
+              :headers="[
+                { key: 'column1', text: 'Column2' },
+                { key: 'column2', text: 'Column2' },
+              ]"
+              :data="[
+                { column1: 'Table', column2: 'Data2' },
+                { column1: 'Data3', column2: 'Data4' },
+              ]"
+            ></sp-table>
+          </div>
+          <div class="margin">
+            <sp-tree-item
+              :itemSync="{
+                id: '1',
+                name: 'Root',
+                children: [
+                  { id: '2', name: 'Child', children: [] },
+                  { id: '3', name: 'Child', children: [] },
+                ],
+              }"
+            >
+              <template v-slot="{ item }">
+                {{ item.name }}
+              </template>
+            </sp-tree-item>
+          </div>
+        </sp-split-view-pane>
+        <sp-split-view-splitter :gripper="true" />
+        <sp-split-view-pane style="padding: 8px">
+          <div class="margin">
+            <sp-color-area />
+          </div>
+          <div class="margin">
+            <sp-color-slider />
+          </div>
+          <div class="margin">
+            <sp-slider />
+          </div>
+        </sp-split-view-pane>
+      </sp-split-view>
+    </div>
+    <div>
       <h2>Components</h2>
       <ul>
         <li style="margin: 8px" v-for="(link, i) in links" :key="i">
@@ -51,6 +140,9 @@
 <style>
 .emoji {
   font-family: Monaco, "Courier New", monospace, "Apple Color Emoji";
+}
+.margin {
+  margin: 8px;
 }
 </style>
 
