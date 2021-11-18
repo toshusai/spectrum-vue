5fc1a028f9e407f532f770caa59922941785b176
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-18 19:50:25 +0900

    add SpAccordion

---
+++ src/components/SpAccordion.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="spectrum-Accordion"
+    role="region"
+  >
+    <div
+      class="spectrum-Accordion-item"
+      :class="{'is-open':isOpen}"
+      role="presentation"
+    >
+      <h3 class="spectrum-Accordion-itemHeading">
+        <button
+          class="spectrum-Accordion-itemHeader"
+          type="button"
+          aria-expanded="true"
+          @click="()=>isOpen = !isOpen"
+        >
+          <slot name="header" />
+        </button>
+        <svg
+          class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-Accordion-itemIndicator"
+          focusable="false"
+          aria-hidden="true"
+        >
+          <use xlink:href="#spectrum-css-icon-Chevron100" />
+        </svg>
+      </h3>
+
+      <div
+        class="spectrum-Accordion-itemContent"
+        role="region"
+      >
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+@import "@spectrum-css/accordion";
+</style>
+<script lang="ts">
+import Vue from "vue";
+import { Component }from "vue-property-decorator";
+
+@Component({})
+export default class SpAccordion extends Vue {
+    isOpen:boolean = false
+    // @Prop({default: ""})header!:string
+
+}
+</script>
