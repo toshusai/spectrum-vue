d930bd51aadf35f84e9d99967f9e668466d64d61
toshusai <toshusai.net@gmail.com>
2021-11-19 22:29:12 +0900

    Add SpCoachMark

---
+++ src/components/SpCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>TODO</div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class SpCard extends Vue {}
+</script>
