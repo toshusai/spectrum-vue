b4a56d9fd543ac15248100cbf5a99dc4e3a960c4
toshusai <toshusai.net@gmail.com>
2021-11-19 23:21:35 +0900

    Add SpDial

---
+++ src/components/SpDial.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="spectrum-Dial"
+    :class="[
+      small ? 'spectrum-Dial--small' : '',
+      disabled ? 'is-disabled' : '',
+    ]"
+  >
+    <div class="spectrum-Dial-controls">
+      <div
+        v-if="label"
+        class="spectrum-Dial-labelContainer"
+      >
+        <label
+          id="dialLabel"
+          class="spectrum-Dial-label"
+          for="labeledDial"
+        >
+          {{ label }}
+        </label>
+        <div
+          v-if="showValue"
+          class="spectrum-Dial-value"
+          role="textbox"
+          aria-readonly="true"
+          aria-labelledby="dialLabel"
+        >
+          {{ value }}
+        </div>
+      </div>
+      <div
+        class="spectrum-Dial-handle"
+        tabindex="0"
+      >
+        <input
+          type="range"
+          class="spectrum-Dial-input"
+          v-bind="$attrs"
+          :disabled="disabled"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+@import "@spectrum-css/dial";
+</style>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class SpDial extends Vue {
+  @Prop({ default: "" }) label!: string;
+  @Prop({ default: 0 }) value!: number;
+  @Prop({ default: false }) small!: boolean;
+  @Prop({ default: false }) disabled!: boolean;
+  @Prop({ default: false }) showValue!: boolean;
+}
+</script>
