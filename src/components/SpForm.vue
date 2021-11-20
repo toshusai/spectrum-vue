82c3bcefdecfca30881e26fea9a9fdc56beb7510
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-20 11:02:20 +0900

    Add SpForm

---
+++ src/components/SpForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form
+    class="spectrum-Form"
+    :class="[labelsAbove ? 'spectrum-Form--labelsAbove' : '']"
+  >
+    <slot />
+  </form>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class SpForm extends Vue {
+  @Prop({ default: false }) labelsAbove!: boolean;
+}
+</script>
---
+++ src/components/SpFormItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <form class="spectrum-Form-item">
+    <slot />
+  </form>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class SpFormItem extends Vue {}
+</script>
