4c9efe705bd341cbd6ede486797316412e00770f
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-20 10:50:21 +0900

    Add SpFieldGroup

---
+++ src/components/SpFieldGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="spectrum-FieldGroup"
+    :class="
+      horizontal
+        ? 'spectrum-FieldGroup--horizontal'
+        : 'spectrum-FieldGroup--vertical'
+    "
+  >
+    <slot />
+  </div>
+</template>
+<style>
+@import "@spectrum-css/fieldgroup";
+</style>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class SpFieldGroup extends Vue {
+  @Prop({ default: true }) horizontal!: boolean;
+}
+</script>
