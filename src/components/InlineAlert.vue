cab1412b70a53c150d3860d9a22c343ea05e4b9a
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-20 11:11:09 +0900

    Add InlineAlert

---
+++ src/components/InlineAlert.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="spectrum-InLineAlert"
+    :class="['spectrum-InLineAlert--' + type]"
+  >
+    <svg
+      class="spectrum-Icon spectrum-Icon--sizeM spectrum-InLineAlert-icon"
+      focusable="false"
+      aria-hidden="true"
+    >
+      <use
+        v-if="type == 'info'"
+        xlink:href="#spectrum-icon-18-Info"
+      />
+      <use
+        v-else-if="type == 'success'"
+        xlink:href="#spectrum-icon-18-CheckmarkCircle"
+      />
+      <use
+        v-else-if="type == 'negative'"
+        xlink:href="#spectrum-icon-18-Alert"
+      />
+      <use
+        v-else-if="type == 'help'"
+        xlink:href="#spectrum-icon-18-Help"
+      />
+      <use
+        v-else-if="type == 'error'"
+        xlink:href="#spectrum-icon-18-Alert"
+      />
+    </svg>
+    <div class="spectrum-InLineAlert-header">
+      <slot name="header" />
+    </div>
+    <div class="spectrum-InLineAlert-content">
+      <slot />
+    </div>
+    <div class="spectrum-InLineAlert-footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class InlineAlert extends Vue {
+  @Prop({ default: "" }) header!: string;
+  @Prop({ default: "info" }) type!: string;
+}
+</script>
