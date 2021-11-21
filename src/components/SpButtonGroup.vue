50de560506de2492c518a797d0eb21bad3d140cc
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-21 22:24:12 +0900

    Add Dialog style in ButtonGroup

--- src/components/SpButtonGroup.vue
+++ src/components/SpButtonGroup.vue
@@ -1,7 +1,11 @@
 <template>
   <div
     class="spectrum-ButtonGroup"
-    :class="vertical ? 'spectrum-ButtonGroup--vertical' : ''"
+    :class="[
+      vertical ? 'spectrum-ButtonGroup--vertical' : '',
+      dialog ? 'spectrum-Dialog-buttonGroup' : '',
+      dialog ? 'spectrum-Dialog-buttonGroup--noFooter' : '',
+    ]"
   >
     <slot />
   </div>
@@ -16,5 +20,6 @@ import { Component, Prop } from "vue-property-decorator";
 @Component({})
 export default class SpButtonGroup extends Vue {
   @Prop({ default: false }) vertical!: boolean;
+  @Prop({ default: false }) dialog!: boolean;
 }
 </script>
