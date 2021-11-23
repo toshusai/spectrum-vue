47076abedb0f0fffb3368e0f8597cca2934f058b
toshusai <35446607+toshusai@users.noreply.github.com>
2021-11-23 11:31:27 +0900

    Update SpActionMenu docs

--- docs/pages/action-menu.vue
+++ docs/pages/action-menu.vue
@@ -1,6 +1,17 @@
 <template>
   <component-view name="ActionMenu" :propData="propData" :eventData="eventData">
+    <h3>Standard</h3>
     <sp-action-menu></sp-action-menu>
+    <sp-action-menu>ActionMenu</sp-action-menu>
+    <h3>size</h3>
+    <sp-action-menu size="S"></sp-action-menu>
+    <sp-action-menu size="S">Size S</sp-action-menu>
+    <h3>size</h3>
+    <sp-action-menu :items="items">
+      <template #item="{ item }">
+        {{ item }}
+      </template>
+    </sp-action-menu>
   </component-view>
 </template>
 <script lang="ts">
@@ -10,5 +21,7 @@ import { Component } from "vue-property-decorator";
 export default class ActionMenuPage extends Vue {
   propData = [];
   eventData = [];
+
+  items = [{ text: "Custom", action: () => {} }];
 }
 </script>
