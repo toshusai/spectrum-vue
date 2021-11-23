16779d91995403fc8fbc3b9477157e17dfdd26bc
toshusai <toshusai.net@gmail.com>
2021-11-23 12:46:26 +0900

    Update action-button

--- docs/pages/action-button.vue
+++ docs/pages/action-button.vue
@@ -8,6 +8,7 @@
     <sp-action-button size="S">S</sp-action-button>
     <sp-action-button size="M">M</sp-action-button>
     <sp-action-button size="L">L</sp-action-button>
+    <code-view :code="code1" />
 
     <div v-for="(t, i) in title" :key="i">
       <h3>{{ t.title }}</h3>
@@ -22,6 +23,7 @@
       >
         {{ x.text }}
       </sp-action-button>
+      <code-view :code="t.code" />
     </div>
   </component-view>
 </template>
@@ -44,6 +46,10 @@ export default class index extends Vue {
     { event: "pointerdown", arg: "Event" },
   ];
 
+  code1 = `<sp-action-button size="S">S</sp-action-button>
+<sp-action-button size="M">M</sp-action-button>
+<sp-action-button size="L">L</sp-action-button>`;
+
   emp = [
     { text: "default", selected: false, disabled: false },
     { text: "selected", selected: true, disabled: false },
@@ -52,16 +58,49 @@ export default class index extends Vue {
   ];
 
   title = [
-    { title: "Standard", quiet: false, emphasized: false },
-    { title: "Quiet", quiet: true, emphasized: false },
-    { title: "Emphasized", quiet: false, emphasized: true },
-    { title: "Emphasized + Quiet", quiet: true, emphasized: true },
+    {
+      title: "Standard",
+      quiet: false,
+      emphasized: false,
+      code: `<sp-action-button>default</sp-action-button>
+<sp-action-button :selected="true">selected</sp-action-button>
+<sp-action-button :disabled="true">disabled</sp-action-button>
+<sp-action-button :selected="true" :disabled="true">selected + disabled</sp-action-button>`,
+    },
+    {
+      title: "Quiet",
+      quiet: true,
+      emphasized: false,
+      code: `<sp-action-button :quiet="true">default</sp-action-button>
+<sp-action-button :quiet="true" :selected="true">selected</sp-action-button>
+<sp-action-button :quiet="true" :disabled="true">disabled</sp-action-button>
+<sp-action-button :quiet="true" :selected="true" :disabled="true">selected + disabled</sp-action-button>`,
+    },
+    {
+      title: "Emphasized",
+      quiet: false,
+      emphasized: true,
+      code: `<sp-action-button :emphasized="true">default</sp-action-button>
+<sp-action-button :emphasized="true" :selected="true">selected</sp-action-button>
+<sp-action-button :emphasized="true" :disabled="true">disabled</sp-action-button>
+<sp-action-button :emphasized="true" :selected="true" :disabled="true">selected + disabled</sp-action-button>`,
+    },
+    {
+      title: "Emphasized + Quiet",
+      quiet: true,
+      emphasized: true,
+      code: `<sp-action-button :quiet="true" :emphasized="true">default</sp-action-button>
+<sp-action-button :quiet="true" :emphasized="true" :selected="true">selected</sp-action-button>
+<sp-action-button :quiet="true" :emphasized="true" :disabled="true">disabled</sp-action-button>
+<sp-action-button :quiet="true" :emphasized="true" :selected="true" :disabled="true">selected + disabled</sp-action-button>`,
+    },
   ].map((x) => {
     return {
       title: x.title,
       items: this.emp,
       quiet: x.quiet,
       emphasized: x.emphasized,
+      code: x.code,
     };
   });
 }
