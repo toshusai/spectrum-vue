8f86f43d5099a345dd07b96988f68221dccef93b
toshusai <toshusai.net@gmail.com>
2021-11-23 22:59:04 +0900

    Add document generator

--- scripts/generateTemplate.js
+++ scripts/generateTemplate.js
@@ -4,17 +4,120 @@
 const fs = require("fs");
 const Case = require("case")
 
+const ts = require("typescript")
+
 const dir = fs.readdirSync("../src/components")
+
+
+
+/**
+ * Get slots from template.
+ * @param src vue template src.
+ * @returns slot names in template
+ */
+function getSlots(src) {
+    const slots = []
+    const slotReg = new RegExp(/<slot.*name="(.*?)".*\/>?/mg)
+    var match = slotReg.exec(src)
+    while (match != null) {
+        slots.push(match[1])
+        match = slotReg.exec(src)
+    }
+    if (src.match(/<slot \/>/)) {
+        slots.push("default")
+    }
+    return slots
+}
+
+function escapeHTML(str) {
+    return str.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+
+function getComponentInfo(src) {
+    const slots = getSlots(src)
+    // get typescript src from vue template
+    const reg = new RegExp(/<script lang="ts">(.*)<\/script>/, 's')
+    src = src.match(reg)
+
+    const sourceFile = ts.createSourceFile(
+        "main.ts",
+        src[1],
+        ts.ScriptTarget.Latest,
+        true
+    );
+
+    const fullText = sourceFile.getFullText()
+
+    let doc = ""
+    ts.forEachChild(sourceFile, (node => {
+        let ranges = ts.getLeadingCommentRanges(fullText, node.getFullStart())
+        if (ranges && ranges.length > 0) {
+            const range = ranges[0]
+            const comment = fullText.substr(range.pos, range.end - range.pos)
+            /**
+             *  
+             */
+            // remove above comment syntax.
+            doc = comment.split("\n").
+                filter(x => x == " *" || x.startsWith(" * ")).map(x => x == " *" ? "" : x.replace(" * ", "")).join('\n')
+            const reg = new RegExp(/<code>(.*?)<\/code>/s)
+            var match = reg.exec(doc)
+            while (match) {
+                let t = escapeHTML(match[1]).trimStart().trimEnd()
+                doc = doc.replace(match[0], match[1] + `<code-view :code="\`${t}\`"></code-view>`)
+                match = reg.exec(doc)
+            }
+        }
+    }))
+
+    const props = []
+    sourceFile.statements.forEach(s => {
+        if (!ts.isClassDeclaration(s)) return
+        s.members.forEach(m => {
+            if (!ts.isPropertyDeclaration(m)) return
+            m.decorators?.forEach(dec => {
+                if (!ts.isDecorator(dec)) return
+                const exp = dec.expression
+                if (!ts.isCallExpression(exp)) return
+                const decFuncName = exp.expression.getText(sourceFile)
+                if (decFuncName != "Prop" && decFuncName != "PropSync") return;
+                const a = exp.arguments[0]
+                if (!ts.isObjectLiteralExpression(a)) return
+                a.properties.forEach(p => {
+                    if (!ts.isPropertyAssignment(p)) return
+                    props.push({
+                        prop: m.name.text,
+                        default: p.initializer.getText(sourceFile),
+                        type: m.type.getText(sourceFile),
+                    })
+                })
+            })
+        })
+    })
+
+    return {
+        slots: slots,
+        props: props,
+        doc: doc
+    }
+}
 
 const slotKeyword = `__SLOT`
 const classNameKeyword = `__CLASSNAME`
-
+const PROP_DATA = `__PROP_DATA`
+const SLOT_DATA = `__SLOT_DATA`
 const template = `
 <template>
     <component-view
     name="${classNameKeyword}"
     :propData="propData"
     :eventData="eventData"
+    :slotData="slotData"
     >
         ${slotKeyword}
     </component-view>
@@ -24,7 +127,8 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 @Component({})
 export default class ${classNameKeyword}Page extends Vue {
-  propData = [];
+  propData = ${PROP_DATA};
+  slotData = ${SLOT_DATA};
   eventData = [];
 }
 </script>
@@ -36,14 +140,19 @@ for (let i = 0; i < dir.length; i++) {
     exportNames.push(name)
 }
 
-exportNames.map(name=>{
+exportNames.map(name => {
     const kebab = Case.kebab(name)
-    const path =`../docs/pages/${kebab}.vue` 
-    if(fs.existsSync(path)){
-        console.warn(`Already exists ${path}`);
+    const path = `../docs/pages/${kebab}.vue`
+    if (name != "ActionButton" && fs.existsSync(path)) {
+        // console.warn(`Already exists ${path}`);
         return
     }
-    let src = template.replace(slotKeyword, `<${kebab}></${kebab}>`)
-    src = src.replace(new RegExp(classNameKeyword, 'g'), name)
-    fs.writeFileSync(path, src)
+    const src = fs.readFileSync(`../src/components/Sp${name}.vue`).toString()
+    const res = getComponentInfo(src)
+    console.log(res);
+    let docSrc = template.replace(slotKeyword, res.doc)
+    docSrc = docSrc.replace(new RegExp(classNameKeyword, 'g'), name)
+    docSrc = docSrc.replace(new RegExp(PROP_DATA, 'g'), JSON.stringify(res.props))
+    docSrc = docSrc.replace(new RegExp(SLOT_DATA, 'g'), JSON.stringify(res.slots.map(s => ({ name: s }))))
+    fs.writeFileSync(path, docSrc)
 })
