eb228062cfe88077d8e331ff1ed33c62f6ba531c
toshusai <toshusai.net@gmail.com>
2021-11-23 12:34:54 +0900

    Add Accordion docs

--- docs/pages/accordion.vue
+++ docs/pages/accordion.vue
@@ -1,6 +1,12 @@
 <template>
-  <component-view name="Accordion" :propData="propData" :eventData="eventData">
-    <sp-accordion></sp-accordion>
+  <component-view
+    name="Accordion"
+    :propData="propData"
+    :eventData="eventData"
+    :slotData="slotData"
+  >
+    <sp-accordion header="Accordion"> Here is Content. </sp-accordion>
+    <code-view :code="code" />
   </component-view>
 </template>
 <script lang="ts">
@@ -8,7 +14,9 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 @Component({})
 export default class AccordionPage extends Vue {
-  propData = [];
+  propData = [{ prop: "header", type: "string", default: "''" }];
   eventData = [];
+  slotData = [{ name: "default" }];
+  code = `<sp-accordion header="Accordion"> Here is Content. </sp-accordion>`;
 }
 </script>
