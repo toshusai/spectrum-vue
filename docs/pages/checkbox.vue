c255be742ec70ecf94166bbd7228138745e2937f
toshusai <toshusai.net@gmail.com>
2021-11-24 22:05:25 +0900

    Add checkbox docs

--- docs/pages/checkbox.vue
+++ docs/pages/checkbox.vue
@@ -7,6 +7,41 @@
     :slotData="slotData"
     >
         
+<sp-checkbox>Checkbox</sp-checkbox>
+<code-view :code="`&lt;sp-checkbox&gt;Checkbox&lt;/sp-checkbox&gt;`"></code-view>
+
+<sp-checkbox :emphasized="true">Checkbox</sp-checkbox>
+<code-view :code="`&lt;sp-checkbox :emphasized=&quot;true&quot;&gt;Checkbox&lt;/sp-checkbox&gt;`"></code-view>
+
+<sp-checkbox :indeterminate="true">Checkbox</sp-checkbox>
+<code-view :code="`&lt;sp-checkbox :indeterminate=&quot;true&quot;&gt;Checkbox&lt;/sp-checkbox&gt;`"></code-view>
+
+<sp-checkbox :invalid="true">Checkbox</sp-checkbox>
+<code-view :code="`&lt;sp-checkbox :invalid=&quot;true&quot;&gt;Checkbox&lt;/sp-checkbox&gt;`"></code-view>
+
+<sp-checkbox :disabled="true">Checkbox</sp-checkbox>
+<code-view :code="`&lt;sp-checkbox :disabled=&quot;true&quot;&gt;Checkbox&lt;/sp-checkbox&gt;`"></code-view>
+
+<h3>Size</h3>
+
+<div>
+  <sp-checkbox size="S">Checkbox</sp-checkbox>
+</div>
+<div>
+  <sp-checkbox size="M">Checkbox</sp-checkbox>
+</div>
+<div>
+  <sp-checkbox size="L">Checkbox</sp-checkbox>
+</div>
+<code-view :code="`&lt;div&gt;
+  &lt;sp-checkbox size=&quot;S&quot;&gt;Checkbox&lt;/sp-checkbox&gt;
+&lt;/div&gt;
+&lt;div&gt;
+  &lt;sp-checkbox size=&quot;M&quot;&gt;Checkbox&lt;/sp-checkbox&gt;
+&lt;/div&gt;
+&lt;div&gt;
+  &lt;sp-checkbox size=&quot;L&quot;&gt;Checkbox&lt;/sp-checkbox&gt;
+&lt;/div&gt;`"></code-view>
     </component-view>
 </template>
 <script lang="ts">
