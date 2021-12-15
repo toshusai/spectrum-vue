b5ca7a90c0d722d5221fbb03c527dddb2429bb62
toshusai <toshusai.net@gmail.com>
2021-12-15 23:21:55 +0900

    Add treeview

---
+++ src/components/SpTreeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul
+    class="spectrum-TreeView"
+    :class="[thumbnail ? 'spectrum-TreeView--thumbnail' : '', 'spectrum-TreeView--size' + size]"
+  >
+    <slot />
+  </ul>
+</template>
+
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({ })
+export default class SpListItemList extends Vue {
+  @Prop({ default: '' }) thumbnail!: string;
+}
+</script>
