e280f4754cdf21c5e0899dc83006be7749037a2c
toshusai <35446607+toshusai@users.noreply.github.com>
2021-12-29 15:22:35 +0900

    Add vertical divider

--- docs/pages/divider.vue
+++ docs/pages/divider.vue
@@ -7,6 +7,32 @@
     :slotData="slotData"
     >
         
+<div style="border: 1px orange solid; padding: 8px">
+  <sp-divider />
+</div>
+<code-view :code="`&lt;div style=&quot;border: 1px orange solid; padding: 8px&quot;&gt;
+  &lt;sp-divider /&gt;
+&lt;/div&gt;`"></code-view>
+
+
+<sp-action-group :compact="true">
+  <sp-action-button :quiet="true">
+    <sp-icon name="Plus" />
+  </sp-action-button>
+  <sp-divider :vertical="true" />
+  <sp-action-button :quiet="true">
+    <sp-icon name="Brush" />
+  </sp-action-button>
+</sp-action-group>
+<code-view :code="`&lt;sp-action-group :compact=&quot;true&quot;&gt;
+  &lt;sp-action-button :quiet=&quot;true&quot;&gt;
+    &lt;sp-icon name=&quot;Plus&quot; /&gt;
+  &lt;/sp-action-button&gt;
+  &lt;sp-divider :vertical=&quot;true&quot; /&gt;
+  &lt;sp-action-button :quiet=&quot;true&quot;&gt;
+    &lt;sp-icon name=&quot;Brush&quot; /&gt;
+  &lt;/sp-action-button&gt;
+&lt;/sp-action-group&gt;`"></code-view>
     </component-view>
 </template>
 <script lang="ts">
@@ -15,7 +41,7 @@ import { Component } from "vue-property-decorator";
 
 @Component({})
 export default class DividerPage extends Vue {
-  propData = [];
+  propData = [{"prop":"vertical","type":"boolean","default":"false"}];
   slotData = [];
   eventData = [];
 
--- src/components/SpDivider.vue
+++ src/components/SpDivider.vue
@@ -1,13 +1,40 @@
 <template>
-  <hr class="spectrum-Divider spectrum-Divider--sizeM">
+  <component
+    :is="vertical ? 'div': 'hr'"
+    class="spectrum-Divider spectrum-Divider--sizeM"
+    :class="[vertical ? 'spectrum-Divider--vertical' : '']"
+    :style="vertical ? 'height: auto; align-self: stretch;' : ''"
+  />
 </template>
 
 
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
 
 @Component({})
-export default class SpDivider extends Vue {}
+export default class SpDivider extends Vue {
+
+  @Prop({default: false}) vertical!: boolean
+}
+/**
+<code>
+<div style="border: 1px orange solid; padding: 8px">
+  <sp-divider />
+</div>
+</code>
+
+<code>
+<sp-action-group :compact="true">
+  <sp-action-button :quiet="true">
+    <sp-icon name="Plus" />
+  </sp-action-button>
+  <sp-divider :vertical="true" />
+  <sp-action-button :quiet="true">
+    <sp-icon name="Brush" />
+  </sp-action-button>
+</sp-action-group>
+</code>
+ */
 </script>
