86ee223e31a121ffcade094ba2d07d5ae0862f91
toshusai <35446607+toshusai@users.noreply.github.com>
2021-12-29 15:34:12 +0900

    Update item-list-item docs

--- docs/pages/item-list-item.vue
+++ docs/pages/item-list-item.vue
@@ -7,6 +7,16 @@
     :slotData="slotData"
     >
         
+<sp-item-list-item :items="['A', 'B', 'C']" selectedItem="A"> 
+  <template v-slot="{item}">
+    {{item}}
+  </template>
+</sp-item-list-item>
+<code-view :code="`&lt;sp-item-list-item :items=&quot;['A', 'B', 'C']&quot; selectedItem=&quot;A&quot;&gt; 
+  &lt;template v-slot=&quot;{item}&quot;&gt;
+    {{item}}
+  &lt;/template&gt;
+&lt;/sp-item-list-item&gt;`"></code-view>
     </component-view>
 </template>
 <script lang="ts">
