5a28b4f30b9aa4cb09bcebdfea8c6baeaed5ac52
toshusai <toshusai.net@gmail.com>
2021-12-29 15:22:43 +0900

    Generate docs

---
+++ docs/pages/tree-view-item.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <component-view
+    name="TreeViewItem"
+    :propData="propData"
+    :eventData="eventData"
+    :slotData="slotData"
+    >
+        
+    </component-view>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class TreeViewItemPage extends Vue {
+  propData = [{"prop":"thumbnail","type":"string","default":"''"},{"prop":"selected","type":"boolean","default":"false"},{"prop":"size","type":"string","default":"\"M\""}];
+  slotData = [{"name":"default"}];
+  eventData = [];
+
+  
+}
+</script>
---
+++ docs/pages/tree-view.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <component-view
+    name="TreeView"
+    :propData="propData"
+    :eventData="eventData"
+    :slotData="slotData"
+    >
+        
+    </component-view>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({})
+export default class TreeViewPage extends Vue {
+  propData = [{"prop":"thumbnail","type":"string","default":"''"},{"prop":"size","type":"string","default":"\"M\""}];
+  slotData = [{"name":"default"}];
+  eventData = [];
+
+  
+}
+</script>
